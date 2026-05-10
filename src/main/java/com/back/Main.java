429fc164fc1a6cd0c11ba35f878d77a9b9f76fd6
oxcm07 <oxcm0727@gmail.com>
2026-05-10 20:27:31 +0900

    008

---
+++ src/main/java/com/back/Main.java
@@ -0,0 +1,51 @@
+package com.back;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //lab1();
+        lab2();
+    }
+
+    private static void lab1() {
+        String input = """
+                등록
+                나의 죽음을 적들에게 알리지 말라.
+                """;
+
+        Scanner scanner = new Scanner(input);
+
+        String cmd = scanner.nextLine();
+        String content = scanner.nextLine();
+
+        System.out.println("cmd : " + cmd);
+        System.out.println("content : " + content);
+    }
+
+    private static void lab2() {
+        System.out.println("안녕하세요.");
+
+        // 기존의 output 스트림 저장
+        PrintStream ORIGINAL_OUT = System.out;
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(byteArrayOutputStream, true);
+        System.setOut(printStream);
+
+        System.out.println("반갑습니다.");
+        String str1 = byteArrayOutputStream.toString(); //반갑습니다.
+
+        System.out.println("좋아요. 하하");
+        String str2 = byteArrayOutputStream.toString(); //반갑습니다.\n좋아요. 하하
+
+        // 기존의 output 스트림 복원
+        System.setOut(ORIGINAL_OUT);
+        printStream.close();
+
+        System.out.println("str1 : " + str1);
+        System.out.println("str2 : " + str2);
+    }
+}
---
+++ src/test/java/com/back/AppTest.java
@@ -0,0 +1,17 @@
+package com.back;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest {
+    @Test
+    @DisplayName("`== 명언 앱 ==` 출력")
+    void t1() {
+        String rs = AppTestRunner.run("종료");
+
+        assertThat(rs)
+                .contains("== 명언 앱 ==");
+    }
+}
