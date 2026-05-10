2d27755ab3999a841ba797e05bcc13602bf7359c
oxcm07 <oxcm0727@gmail.com>
2026-05-10 21:24:27 +0900

    015

--- src/main/java/com/back/domain/wiseSaying/repository/WiseSayingRepository.java
+++ src/main/java/com/back/domain/wiseSaying/repository/WiseSayingRepository.java
@@ -1,4 +1,23 @@
 package com.back.domain.wiseSaying.repository;
 
+import com.back.domain.wiseSaying.entity.WiseSaying;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class WiseSayingRepository {
+    private final List<WiseSaying> wiseSayings = new ArrayList<>();
+    private int index = 1;
+
+    public WiseSaying save(WiseSaying wiseSaying) {
+        wiseSaying.setId(index++);
+
+        wiseSayings.add(wiseSaying);
+
+        return wiseSaying;
+    }
+
+    public List<WiseSaying> findForList() {
+        return wiseSayings.reversed();
+    }
 }
--- src/main/java/com/back/domain/wiseSaying/service/WiseSayingService.java
+++ src/main/java/com/back/domain/wiseSaying/service/WiseSayingService.java
@@ -1,4 +1,26 @@
 package com.back.domain.wiseSaying.service;
 
+import com.back.domain.wiseSaying.entity.WiseSaying;
+import com.back.domain.wiseSaying.repository.WiseSayingRepository;
+
+import java.util.List;
+
 public class WiseSayingService {
+    private final WiseSayingRepository wiseSayingRepository;
+
+    public WiseSayingService() {
+        this.wiseSayingRepository = new WiseSayingRepository();
+    }
+
+    public WiseSaying write(String content, String author) {
+        WiseSaying wiseSaying = new WiseSaying(content, author);
+
+        wiseSayingRepository.save(wiseSaying);
+
+        return wiseSaying;
+    }
+
+    public List<WiseSaying> findForList() {
+        return wiseSayingRepository.findForList();
+    }
 }
