46ab57f29454b0ac01bfdb14ec16878e6581eea2
oxcm07 <oxcm0727@gmail.com>
2026-05-10 20:47:15 +0900

    011

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -11,5 +11,28 @@ public class App {
 
     public void run() {
         System.out.println("== 명언 앱 ==");
+        int index = 1;
+
+        while(true) {
+            System.out.println("명령) ");
+            String cmd = scanner.nextLine();
+
+            switch (cmd) {
+                case "등록" -> {
+                    System.out.println("명언 : ");
+                    String content = scanner.nextLine();
+
+                    System.out.println("작가 : ");
+                    String author = scanner.nextLine();
+
+                    int id = index++;
+
+                    System.out.printf("%d번 명언이 등록되었습니다.\n",id);
+                }
+                case "종료" -> {
+                    return;
+                }
+            }
+        }
     }
 }
--- src/test/java/com/back/AppTest.java
+++ src/test/java/com/back/AppTest.java
@@ -9,9 +9,55 @@ public class AppTest {
     @Test
     @DisplayName("`== 명언 앱 ==` 출력")
     void t1() {
-        String rs = AppTestRunner.run("종료");
+        String rs = AppTestRunner.run("");
 
         assertThat(rs)
                 .contains("== 명언 앱 ==");
+    }
+
+    @Test
+    @DisplayName("등록")
+    void t2() {
+        String rs = AppTestRunner.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                종료
+                """);
+
+        assertThat(rs)
+                .contains("명령) ")
+                .contains("명언 : ")
+                .contains("작가 : ");
+    }
+
+    @Test
+    @DisplayName("등록시 생성된 명언번호 노출")
+    void t3() {
+        String rs = AppTestRunner.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
+
+        assertThat(rs)
+                .contains("1번 명언이 등록되었습니다.");
+    }
+
+    @Test
+    @DisplayName("등록할때 마다 생성되는 명언번호가 증가")
+    void t4() {
+        String rs = AppTestRunner.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
+
+        assertThat(rs)
+                .contains("1번 명언이 등록되었습니다.")
+                .contains("2번 명언이 등록되었습니다.");
     }
 }
